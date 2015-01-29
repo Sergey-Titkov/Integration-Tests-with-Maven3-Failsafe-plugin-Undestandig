bec4268900f6e0467e2f5eca6e96f37387807196
Sergey.Titkov <Sergey.Titkov@billing.ru>
2015-01-29 18:48:39 +0300

    Проект подготовлен. Структура  создана. Модульный тест отработал.

---
+++ integration-test-with-unit-tests/src/main/java/SimpleClass.java
@@ -0,0 +1,12 @@
+/**
+ * Описание
+ *
+ * @author Sergey.Titkov
+ * @version 001.00
+ * @since 001.00
+ */
+public class SimpleClass {
+  public String getText(){
+    return "Это текст";
+  }
+}
---
+++ integration-test-with-unit-tests/src/test/java/SimpleClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleClassTest {
+
+  @Test
+  public void testGetText() throws Exception {
+    assertTrue(true);
+  }
+}
